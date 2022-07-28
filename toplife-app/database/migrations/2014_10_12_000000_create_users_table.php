34b93bfa6a4b3078af70c116f83d017517e97e56
felipe3684 <g98felipe@gmail.com>
2022-07-28 21:01:46 +0200

    reg/login ok

--- toplife-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ toplife-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('surnames');
+            $table->boolean('isadmin')->nullable();
+            $table->date('datebirth');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
